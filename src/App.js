d9cdaf8038a94c716b1f8113ba516596c69e22f3
maruf14 <xusniddinovmaruf15@gmail.com>
2021-07-15 17:29:42 +0500

    div

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import { faQuoteRight } from '@fortawesome/free-solid-svg-icons';
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <div className="row d-flex  justify-content-center">
+      <div className="container py-5 ">
+        <div className="row d-flex  justify-content-center ">
 
           <div className="col-md-4">
             <Card icon={faQuoteRight} title="JOHN DOE" img={'https://www.cashgamefestival.com/wp-content/uploads/2014/10/speaker-2-v2.jpg'} />
@@ -29,6 +29,10 @@ function App() {
 
 
 
+        </div>
+        <div className="text-center mt-4">
+          <p className="text-dark fs-1 mb-3">RESPONSIVE <span className="fw-bold">TESTIMONALS</span> CARD</p>
+          <p className="text-dark fs-5 mt-2">UI DESIGN USING <span className="fw-bold">HTML</span> & <span className="fw-bold">CSS </span> ONLY</p>
         </div>
       </div>
 
